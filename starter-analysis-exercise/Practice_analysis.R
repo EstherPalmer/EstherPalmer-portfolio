ff1d7e4f75b97f66a69d1bec90d3781082ba9c69
Esther Palmer <estherpalmer253@gmail.com>
2026-01-20 23:27:37 -0500

    identifying missing data

--- starter-analysis-exercise/Practice_analysis.R
+++ starter-analysis-exercise/Practice_analysis.R
@@ -34,3 +34,25 @@ pop_LE <- data.frame(africadata$population, africadata$life_expectancy)
 #This creates a new dataframe for population and life expectancy data
 str(pop_LE)
 #This checks that I have everything right
+
+#is.na(africadata)
+#this creates a matrix of true false values where True=NA
+
+#is.na(africadata$infant_mortality)
+#lists NA in the vector africadata$infant_mortality
+
+year_inf <- data.frame(africadata$year, africadata$infant_mortality)
+#this creates a matrix with year and infant mortality
+#complete.cases(year_inf)
+#this lists which rows have a missing value
+#year_inf2 <- year_inf[complete.cases(year_inf),]
+#this removes all of the rows with missing data, which I need to list them instead
+#which(is.na(year_inf), T)
+#returns a column row pair of what is missing
+year_inf[is.na(year_inf)] <- "A"
+#this line rewrites the dataset, changing all NA's to A
+missing <- subset(year_inf, (africadata.infant_mortality == "A"))
+#this makes a new dataset with the subset function that pulls out all the years with A as a value
+#This didn't work with NA which is why I changed it to A. Not the most elegant solution but it works
+str(missing)
+#this line is for checking
